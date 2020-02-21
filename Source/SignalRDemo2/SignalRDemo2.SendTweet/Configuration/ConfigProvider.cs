1d7fe244f67fb7f4579e4fd3f255202ce156b96f
Jose Ramos Sánchez <jramos@plainconcepts.com>
2020-02-21 23:35:07 +0100

    Initial commit SignalRDemo2.SendTweet

---
+++ Source/SignalRDemo2/SignalRDemo2.SendTweet/Configuration/ConfigProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace SignalRDemo2.SendTweet.Configuration
+{
+    public class ConfigProvider
+    {
+        public static string ConsumerKey => ConfigurationManager.AppSettings["consumerKey"];
+        public static string ConsumerSecret => ConfigurationManager.AppSettings["consumerSecret"];
+        public static string AccessToken => ConfigurationManager.AppSettings["accessToken"];
+        public static string AccessTokenSecret => ConfigurationManager.AppSettings["accessTokenSecret"];
+    }
+}
---
+++ Source/SignalRDemo2/SignalRDemo2.SendTweet/Program.cs
@@ -0,0 +1,33 @@
+﻿using SignalRDemo2.SendTweet.Configuration;
+using System;
+using Tweetinvi;
+
+namespace SignalRDemo2.SendTweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumerKey = ConfigProvider.ConsumerKey;
+            var consumerSecret = ConfigProvider.ConsumerSecret;
+            var accessToken = ConfigProvider.AccessToken;
+            var accessTokenSecret = ConfigProvider.AccessTokenSecret;
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+
+            var user = User.GetAuthenticatedUser();
+
+            var tweet = Tweet.PublishTweet("Hello from tweetinvi");
+
+            var timelineTweets = Timeline.GetUserTimeline(user, 5);
+
+            foreach (var timelineTweet in timelineTweets)
+            {
+                Console.WriteLine(timelineTweet);
+                Console.WriteLine("***********");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
