c963270f5e07bd272ce559906ad9ce16e2249f2a
Jose Ramos Sánchez <jramos@plainconcepts.com>
2020-02-21 23:25:49 +0100

    Initial commit SignalRDemo1

---
+++ Source/SignalRDemo1/SignalRDemo1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemo1.Hubs;
+
+namespace SignalRDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        IHubContext<MessageHub> context;
+        public HomeController(IHubContext<MessageHub> hub)
+        {
+            context = hub;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Para habilitar el poder enviar un mensaje desde la misma vista
+        public IActionResult Message()
+        {
+            return View();
+        }
+        
+        
+        public void AddMessage(string message)
+        {
+            //Para enviar el mensaje a todo el mundo
+            context.Clients.All.SendAsync("new-message", message);
+        
+            //Para enviar el mensaje a un grupo determinado
+            //context.Clients.Group("groupName").SendAsync("new-message", message);
+        
+            //Para enviar el mensaje a un usuario determinado
+            //context.Clients.User("userId").SendAsync("new-message", message);
+        
+            //Para enviar el mensaje a una connectionId determinada
+            //context.Clients.Client("connectionId").SendAsync("new-message", message);
+        
+            //Para enviar el mensaje a todos, menos a la/s connectionId indicadas
+            //context.Clients.AllExcept("excludedConnectionId").SendAsync("new-message", message);                    
+        }
+    }
+}
---
+++ Source/SignalRDemo1/SignalRDemo1/Hubs/MessageHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRDemo1.Hubs
+{
+    public class MessageHub : Hub
+    {
+        private static int users=0;
+
+        //Cuando un usuario se conecta
+        public override Task OnConnectedAsync()
+        {            
+            return Clients.All.SendAsync("users-connected", ++users);
+        }
+
+        //Cuando un usuario se desconecta
+        public override Task OnDisconnectedAsync(Exception exception)
+        {            
+            return Clients.All.SendAsync("users-connected", --users);
+        }
+
+        public Task BroadcastMessage(string message)
+        {
+            return Clients.All.SendAsync("new-message", message);
+        }
+    }
+}
