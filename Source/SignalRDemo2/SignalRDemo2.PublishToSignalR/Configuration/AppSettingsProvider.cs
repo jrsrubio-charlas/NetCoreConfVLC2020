04842de85bc84cc3e58a1980a24becfc3bf9a4d9
Jose Ramos Sánchez <jramos@plainconcepts.com>
2020-03-01 12:34:35 +0100

    Last changes - PublishToSignalR

--- Source/SignalRDemo2/SignalRDemo2.PublishToSignalR/Configuration/AppSettingsProvider.cs
+++ Source/SignalRDemo2/SignalRDemo2.PublishToSignalR/Configuration/AppSettingsProvider.cs
@@ -9,5 +9,7 @@ namespace SignalRDemo2.PublishToSignalR.Configuration
         private static IConfiguration Config => ConfigProvider.GetConfig();
 
         public static string MyTwitterUser => Config.GetValue<string>("MyTwitterUser");
+
+        public static string EhConnectionStringToSendTweet => Config.GetValue<string>("EhConnectionStringToSendTweet");
     }
 }
